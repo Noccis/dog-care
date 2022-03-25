16a47e0633c0abe69466b1d18bc8fc1cc0925561
Noccis <89582846+Noccis@users.noreply.github.com>
2022-03-25 13:44:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const dogRegisterAPI = 'https://api.jsonbin.io/b/623db6bf06182767437fadcf';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dog Care</h1>
       </header>
     </div>
   );
