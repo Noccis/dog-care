b7c708ca347a81d9b4701b43ac243e160bff0ccc
Noccis <89582846+Noccis@users.noreply.github.com>
2022-04-01 15:10:49 +0200

    activeDog created to keep track of what dog is showing on info

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,41 @@
-const Register = () => {
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Register = ({dogList, showInfo, dodo}) => {
+
+  //  console.log(dogList)
+
+    let listOfDogs = null;
+
+    
+    if(dogList != null) {
+
+      listOfDogs = dogList.map(dog => (
+
+          <div key={dog.name} className="dog">
+             
+             <img src={dog.img} alt="Picture of a dog" />  
+             <p>{dog.name}</p>
+             <Link to={"/info"}>
+                <button onClick={() => showInfo(dog)}>Set Dog</button>
+             </Link>
+             
+          </div>   
+        ))
+
+      //  console.log('REGISTER, dogList is not null!' + listOfDogs)
+    }
+
     return (
-        <section>
-            <p>Register</p>
+        <section className="register">
+            <h2>List of our dogs</h2>
+            <button onClick={() => {
+                console.log(dodo);
+            }}>Print Dog</button>
+            <section className="dog-list">
+                {listOfDogs}
+            </section>
+            
         </section>
     )
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,11 @@
-const Welcome = () => {
+const Welcome = (activeDog) => {
     return (
         <section>
             <h1>Welcome to Doggy daycare</h1>
             <p>We love our job!</p>
+            <button onClick={() => {
+                console.log(activeDog.name)
+            }}>print</button>
         </section>
     )
 }
