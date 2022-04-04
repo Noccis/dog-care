c849ff50b20cc860e27b1efe8b8dc5e7422bf0e5
Noccis <89582846+Noccis@users.noreply.github.com>
2022-04-04 12:57:29 +0200

    moved register to own css

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import '../styling/register.css';
 
 const Register = ({dogList, showInfo, dodo}) => {
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
+import dogHeader from '../images/dogsplayingheader2.jpg';
+import '../styling/welcome.css'
+
 const Welcome = () => {
     return (
         <section>
+            <img src={dogHeader} alt="Cute dogs playing" className='header-image'></img>
             <h1>Welcome to Doggy daycare</h1>
-            <p>We love our job!</p>
+            <div className='welcome-information'>
+            <p>"Quote from happy owner"</p>
+            <p>Here you can see some very important information about our dogcare that is written in a easy and funny way. So good, yeah!</p>
+            </div>
+            
 
         </section>
     )
