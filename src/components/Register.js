60a853b3f71cf4ee2966d1abe2dd62720cf536fd
Noccis <89582846+Noccis@users.noreply.github.com>
2022-04-04 15:36:55 +0200

    started on register design

--- src/components/Register.js
+++ src/components/Register.js
@@ -19,7 +19,7 @@ const Register = ({dogList, showInfo, dodo}) => {
              <img src={dog.img} alt="Picture of a dog" />  
              <p>{dog.name}</p>
              <Link to={"/info"}>
-                <button onClick={() => showInfo(dog)}>Set Dog</button>
+                <button onClick={() => showInfo(dog)}>Show info</button>
              </Link>
              
           </div>   
@@ -31,12 +31,12 @@ const Register = ({dogList, showInfo, dodo}) => {
     return (
         <section className="register">
           <div className='header-image-container'>
-            <img src={dogHeader} className="header-image"/>
+            <img src={dogHeader} className="small-header-image"/>
             <p className="page-info"><span>Our dogs</span> - click a dog for more info</p>
           </div>
-            <button onClick={() => {
+           {/*  <button onClick={() => {
                 console.log(dodo);
-            }}>Print Dog</button>
+            }}>Print Dog</button> */}
             <section className="dog-list">
                 {listOfDogs}
             </section>
