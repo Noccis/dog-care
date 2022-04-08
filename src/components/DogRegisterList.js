c83a405e02689013f78cb9a9d96a7346479bd3da
Noccis <89582846+Noccis@users.noreply.github.com>
2022-04-08 10:02:56 +0200

    api function moved to DogResisterList component

--- src/components/DogRegisterList.js
+++ src/components/DogRegisterList.js
@@ -3,7 +3,7 @@
 // Api for all the dogs attending this daycare
   
 
-async function DogRegisterList({setDogList}) {
+async function DogRegisterList(setDogList) {
 
     console.log("Function running");
     const dogRegisterAPI = 'https://api.jsonbin.io/b/623dc40406182767437fb28d';
