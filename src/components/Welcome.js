81baff81b7fb654f848e0604b4df18c24f222ccf
Noccis <89582846+Noccis@users.noreply.github.com>
2022-04-12 14:32:51 +0200

    media query fixed

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import dogHeader from '../images/dogsplayingheader.jpg';
 import '../styling/welcome.css'
 
+import walkerImage from '../images/dogwalker.jpeg'
+
 const Welcome = () => {
     return (
         <section>
@@ -11,13 +13,12 @@ const Welcome = () => {
                     <p className='slogan'>"Sometimes my beloved Pixma don't want to go home, <br /> 
                      I take that as a good sign."<br/>
                     - Happy dog owner</p>
+                </div>
             </div>
             
-            </div>
-            <h1>Welcome to Doggy daycare</h1>
             <div className='welcome-information'>
-            <p>"Quote from happy owner"</p>
-            <p>Here you can see some very important information about our dogcare that is written in a easy and funny way. So good, yeah!</p>
+            <img src={walkerImage}/>
+            <p><h1>Welcome to Doggy daycare.</h1>Here you can see some very important information about our dogcare that is written in a easy and funny way. So good, yeah!</p>
             </div>
             
 
